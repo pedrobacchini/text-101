4f2c83918e75330c84e0a47d4088669ecbf85468
Pedro Bacchini <pedrobacchini@outlook.com>
2019-09-10 15:28:04 -0300

    Create Project

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] private Text textComponent = null;
+    [SerializeField] private State startingState = null;
+
+    private State _state;
+
+    private void Start()
+    {
+        _state = startingState;
+        textComponent.text = _state.GetStoryText();
+    }
+
+    private void Update()
+    {
+        ManagerState();
+    }
+
+    private void ManagerState()
+    {
+        var nextStates = _state.GetNextStates();
+        for (var indexState = 0; indexState < nextStates.Length; indexState++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + indexState))
+            {
+                _state = nextStates[indexState];
+            }
+        }
+        textComponent.text = _state.GetStoryText();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+     [TextArea(14,10)] [SerializeField] private string storyText = null;
+     [SerializeField] private State[] nextStates = null;
+
+     public string GetStoryText()
+     {
+          return storyText;
+     }
+
+     public State[] GetNextStates()
+     {
+          return nextStates;
+     }
+}
